4fb7a89bfecb388df19a7d0b649a164fd6c2ccf1
itsprithvi <prithvi.myk@gmail.com>
2023-09-18 12:49:13 +0530

    implemented api-gateway

---
+++ api-gateway/src/main/java/org/prithvi/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,15 @@
+package org.prithvi.apigateway;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+}
